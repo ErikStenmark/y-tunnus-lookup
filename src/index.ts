3622a0a342a48cda9bfcdb2182f71401bdc50644
erik <erik.stenmark@visma.com>
2022-06-21 22:28:04 +0300

    fetches data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const url = 'https://avoindata.prh.fi/bis/v1/1080465-1';
+import fetch from 'node-fetch';
 
-console.log(url);
+const fetchBusiness = async (businessId: string) => {
+    const response = await fetch(`https://avoindata.prh.fi/bis/v1/${businessId}`);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    const data = await fetchBusiness('1080465-1');
+    console.log(data);
+})();
